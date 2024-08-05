c9ba31f4af2d5b277ef1e963ca30e419fa99e673
Segment Git Sync <friends@segment.com>
2024-08-05 15:05:37 +0000

    Full sync

---
+++ users/5NJcEQyoGhw6Gkci2fgZGE.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_user.id-5NJcEQyoGhw6Gkci2fgZGE
+  id = "5NJcEQyoGhw6Gkci2fgZGE"
+}
+
+resource "segment_user" "id-5NJcEQyoGhw6Gkci2fgZGE" {
+  email = "talhaa.rabbani@segment.com"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "cWBgjiLvqmfWSiUjyonfVR"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DFMmZbyzdEWmE6k8JVgKAiE"
+    },
+  ]
+}
---
+++ users/sXK7DV41JxPqcRKFpmqkmn.tf
@@ -0,0 +1,87 @@
+import {
+  to = segment_user.id-sXK7DV41JxPqcRKFpmqkmn
+  id = "sXK7DV41JxPqcRKFpmqkmn"
+}
+
+resource "segment_user" "id-sXK7DV41JxPqcRKFpmqkmn" {
+  email = "ancee.liu@segment.com"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "cWBgjiLvqmfWSiUjyonfVR"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DFJmy6Vln5DZyS3SGBXjTdN"
+    },
+    {
+      resources = [
+        {
+          id = "cWBgjiLvqmfWSiUjyonfVR"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DFMmZbyzdEWmE6k8JVgKAiE"
+    },
+    {
+      resources = [
+        {
+          id = "cWBgjiLvqmfWSiUjyonfVR"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DGbc2C28CSKzqm4RCJlYAvx"
+    },
+    {
+      resources = [
+        {
+          id = "cWBgjiLvqmfWSiUjyonfVR"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DKQ95UMoCKw1y9syxModCaC"
+    },
+    {
+      resources = [
+        {
+          id = "cWBgjiLvqmfWSiUjyonfVR"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DLRqfeBpxN5cNBjK0n4oTPX"
+    },
+    {
+      resources = [
+        {
+          id = "cWBgjiLvqmfWSiUjyonfVR"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1ZxNOhjYFrqvYCPreLHBNTnepB4"
+    },
+    {
+      resources = [
+        {
+          id = "cWBgjiLvqmfWSiUjyonfVR"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "2Neg5AUm9pCOWXGQYzKOiBqpvLk"
+    },
+  ]
+}
