d0bdb868e127578cc7398e2112e6f8fed5b33006
Segment Git Sync <friends@segment.com>
2024-08-22 18:28:50 +0000

    Destination '66c78361b121f32dc1d95079' created by andy.cogbill@segment.com

---
+++ destinations/66c78361b121f32dc1d95079.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-66c78361b121f32dc1d95079
+  id = "66c78361b121f32dc1d95079"
+}
+
+resource "segment_destination" "id-66c78361b121f32dc1d95079" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "61957755c4d820be968457de"
+    partner_owned     = false
+    region_endpoints  = ["US"]
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name      = "Salesforce (Actions)"
+  settings  = "{\"auth_password\":\"\",\"isSandbox\":false,\"security_token\":\"\",\"username\":\"\"}"
+  source_id = "2TapZFYdqrQMgoXiU2JZCU"
+}
