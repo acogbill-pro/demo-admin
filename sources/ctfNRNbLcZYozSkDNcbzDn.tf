c693a469f114ec77f903cba6e239ff663d9549da
Segment Git Sync <friends@segment.com>
2024-08-23 14:17:28 +0000

    Source 'ctfNRNbLcZYozSkDNcbzDn' created by ancee.liu@segment.com

---
+++ sources/ctfNRNbLcZYozSkDNcbzDn.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-ctfNRNbLcZYozSkDNcbzDn
+  id = "ctfNRNbLcZYozSkDNcbzDn"
+}
+
+resource "segment_source" "id-ctfNRNbLcZYozSkDNcbzDn" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "IqDTy1TpoU"
+  }
+  name     = "Mercury"
+  settings = "{}"
+  slug     = "mercury"
+}
