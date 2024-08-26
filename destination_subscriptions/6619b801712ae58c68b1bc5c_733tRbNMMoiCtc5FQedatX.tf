573b505feaf1b0d1693a1500bb2cc224a7b940dc
Segment Git Sync <friends@segment.com>
2024-08-26 15:08:10 +0000

    Destination Subscription '6619b801712ae58c68b1bc5c:733tRbNMMoiCtc5FQedatX' disabled by andy.cogbill@segment.com

--- destination_subscriptions/6619b801712ae58c68b1bc5c_733tRbNMMoiCtc5FQedatX.tf
+++ destination_subscriptions/6619b801712ae58c68b1bc5c_733tRbNMMoiCtc5FQedatX.tf
@@ -6,7 +6,7 @@ import {
 resource "segment_destination_subscription" "id-6619b801712ae58c68b1bc5c_733tRbNMMoiCtc5FQedatX" {
   action_id      = "oiFvX4YXwyaPVysi1t7mtz"
   destination_id = "6619b801712ae58c68b1bc5c"
-  enabled        = true
+  enabled        = false
   model_id       = "bgwPiXAwQ2GWHdAvuhev8L"
   name           = "Review with Sentiment"
   settings       = "{\"anonymous_id\":{\"@path\":\"$.properties.USER\"},\"consent\":{\"@path\":\"$.context.consent\"},\"enable_batching\":false,\"event_name\":\"Product Review\",\"message_id\":{\"@path\":\"$.messageId\"},\"properties\":{\"contents\":{\"@path\":\"$.properties.REVIEW\"},\"product_category\":{\"@path\":\"$.properties.PRODUCT_CATEGORY\"},\"product_description\":{\"@path\":\"$.properties.PRODUCT_DESCRIPTION\"},\"product_name\":{\"@path\":\"$.properties.PRODUCT_NAME\"},\"sentiment_score\":{\"@path\":\"$.properties.SENTIMENT_SCORE\"},\"summary\":{\"@path\":\"$.properties.SUMMARY\"},\"will_buy_again\":{\"@path\":\"$.properties.WILL_BUY_AGAIN\"}},\"timestamp\":{\"@path\":\"$.receivedAt\"},\"user_id\":{\"@path\":\"$.properties.USER\"}}"
