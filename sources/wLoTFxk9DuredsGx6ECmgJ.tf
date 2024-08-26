b1c7f2d86e95c44817242a7ec70ff51d86e0de26
Segment Git Sync <friends@segment.com>
2024-08-26 15:59:32 +0000

    Source 'wLoTFxk9DuredsGx6ECmgJ' modified by andy.cogbill@segment.com

---
+++ sources/wLoTFxk9DuredsGx6ECmgJ.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-wLoTFxk9DuredsGx6ECmgJ
+  id = "wLoTFxk9DuredsGx6ECmgJ"
+}
+
+resource "segment_source" "id-wLoTFxk9DuredsGx6ECmgJ" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "BQp3zGUgjV"
+  }
+  name     = "Entity Group: snowflake in spa_dmPAX6ZxK5v2dH9AZz6eEZ"
+  settings = "{\"account\":\"ecb63470.prod3.us-west-2\",\"database\":\"ACOGBILL_DEMO\",\"username\":\"ACOGBILL\",\"warehouse\":\"SEGMENT_WAREHOUSE\"}"
+  slug     = "entity-group-snowflake-spa_dmpax6zxk5v2dh9azz6eez"
+}
