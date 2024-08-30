b777131d88de18e1fa93ba0a11be8ccbcf086498
Segment Git Sync <friends@segment.com>
2024-08-30 17:47:20 +0000

    Destination '66d205a6f042fa3045b74a64' created by andy.cogbill@segment.com

---
+++ destinations/66d205a6f042fa3045b74a64.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-66d205a6f042fa3045b74a64
+  id = "66d205a6f042fa3045b74a64"
+}
+
+resource "segment_destination" "id-66d205a6f042fa3045b74a64" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "5a6b50f1c900fa00011858fd"
+    partner_owned     = false
+    region_endpoints  = null
+    supported_regions = null
+  }
+  name      = "Google Ads Remarketing Lists"
+  settings  = "{\"account\":\"\",\"adPersonalization\":\"\",\"adUserData\":\"\",\"appId\":\"\"}"
+  source_id = "2TapZFYdqrQMgoXiU2JZCU"
+}
