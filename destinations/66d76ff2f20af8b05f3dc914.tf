791e56eb6714418b82bf3572d0183a2b9084277d
Segment Git Sync <friends@segment.com>
2024-09-03 20:22:12 +0000

    Destination '66d76ff2f20af8b05f3dc914' created by ancee.liu@segment.com

---
+++ destinations/66d76ff2f20af8b05f3dc914.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-66d76ff2f20af8b05f3dc914
+  id = "66d76ff2f20af8b05f3dc914"
+}
+
+resource "segment_destination" "id-66d76ff2f20af8b05f3dc914" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "61806e472cd47ea1104885fc"
+    partner_owned     = false
+    region_endpoints  = ["US"]
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name      = "Facebook CAPI"
+  settings  = "{\"pixelId\":\"\",\"testEventCode\":\"\"}"
+  source_id = "mqvvpV9K9sXQWsVEhwoz3c"
+}
